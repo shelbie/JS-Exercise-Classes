8a197109b6295ee93da29f47777412b96ac16232
Shelbie <30550643+shelbie@users.noreply.github.com>
2020-03-19 18:59:27 -0500

    finished person constructor challenge

--- index.js
+++ index.js
@@ -41,6 +41,24 @@ class Airplane {
 */
 
 class Person {
+constructor(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
+eat(someFood) {
+  if(this.stomach.length < 10){
+  return this.stomach.push(someFood);
+  } 
+}
+
+poop() {
+  return this.stomach = [];
+}
+
+toString() {
+  return `${this.name}, and ${this.age}`
+}
 
 }
 
