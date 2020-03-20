04ea6b03356dd0569624afd17d327f150d117acf
Shelbie <30550643+shelbie@users.noreply.github.com>
2020-03-19 19:06:53 -0500

    finished car constructor challenge

--- index.js
+++ index.js
@@ -77,6 +77,27 @@ toString() {
 */
 
 class Car {
+constructor(model, milesPerGallon){
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+
+fill(gallons){
+  return this.tank += gallons;
+}
+
+drive(distance){
+  if(distance / this.milesPerGallon >= this.tank) {
+    this.odometer += this.tank * this.milesPerGallon;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  }
+  this.odometer += distance;
+  this.tank -= distance / this.milesPerGallon;
+  
+};
 
 }
 
